c739d0e29c8e2e536089c7ad7b4402725cc6822b
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-11-16 23:00:46 +0100

    ACUnitRepository added

---
+++ src/main/java/com/komfortcieplny/CRM/ac_unit/repository/ACUnitRepository.java
@@ -0,0 +1,7 @@
+package com.komfortcieplny.CRM.ac_unit.repository;
+
+import com.komfortcieplny.CRM.ac_unit.model.ACUnit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ACUnitRepository extends JpaRepository<ACUnit, Long> {
+}
