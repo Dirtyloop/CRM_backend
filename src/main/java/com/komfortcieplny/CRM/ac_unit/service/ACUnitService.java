6ebcd162031ebaf55a0c9a792175f90486acb54a
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-11-16 23:01:03 +0100

    ACUnitService added

---
+++ src/main/java/com/komfortcieplny/CRM/ac_unit/service/ACUnitService.java
@@ -0,0 +1,13 @@
+package com.komfortcieplny.CRM.ac_unit.service;
+
+import com.komfortcieplny.CRM.ac_unit.repository.ACUnitRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ACUnitService {
+    private final ACUnitRepository acUnitRepository;
+
+    public ACUnitService(ACUnitRepository acUnitRepository) {
+        this.acUnitRepository = acUnitRepository;
+    }
+}
